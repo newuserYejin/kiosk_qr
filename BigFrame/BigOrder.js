823e1a36fa6e607934420e2e862914a12dcd4eac
jin <jin200126@gmail.com>
2023-08-05 19:57:23 +0900

    2023_8_5

--- BigFrame/BigOrder.js
+++ BigFrame/BigOrder.js
@@ -16,22 +16,52 @@ const joImage = document.getElementById("imageLink");
 
 joImage.addEventListener("click", function () {
   // 먼저 모달 컨테이너를 비웁니다.
-  document.getElementById("menu_modalContainer").innerHTML = "";
+  document.getElementById("modalContainer").innerHTML = "";
+
+  // detail_menu.css를 제거합니다.
+  const detailMenuLink = document.querySelector('link[href="../detail_menu/detail_menu.css"]');
+  if (detailMenuLink) {
+    detailMenuLink.remove();
+  }
 
   // help_msg.html 콘텐츠를 로드하여 모달 컨테이너에 추가합니다.
-  $.get("../help_msg/help_msg.html", function (data) {
-    $("#menu_modalContainer").html(data); // "menu_modalContainer"로 변경
-    const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
-    modal.show();
-  });
-});
+  fetch("../help_msg/help_msg.html")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("HTTP Error " + response.status);
+      }
+      return response.text();
+    })
+    .then(data => {
+      // 모달 컨테이너에 help_msg.html 콘텐츠를 추가합니다.
+      $("#modalContainer").html(data);
 
+      // help_msg.css 파일을 로드합니다.
+      const linkElement = document.createElement("link");
+      linkElement.rel = "stylesheet";
+      linkElement.type = "text/css";
+      linkElement.href = "../help_msg/help_msg.css";
+      document.head.appendChild(linkElement);
+
+      const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
+      modal.show();
+    })
+    .catch(error => {
+      console.error("콘텐츠를 가져오는 중 오류가 발생했습니다:", error);
+    });
+});
 // 선택 버튼
 
 const selectBtn = document.getElementById("selectBtn");
 selectBtn.addEventListener("click", function () {
   // 먼저 모달 컨테이너를 비웁니다.
-  document.getElementById("menu_modalContainer").innerHTML = "";
+  document.getElementById("modalContainer").innerHTML = "";
+
+  // help_msg.css를 제거합니다.
+  const detailMenuLink = document.querySelector('link[href="../help_msg/help_msg.css"]');
+  if (detailMenuLink) {
+    detailMenuLink.remove();
+  }
 
   // jojo.html 콘텐츠를 로드하여 모달 컨테이너에 추가합니다.
   fetch("../detail_menu/jojo.html")
@@ -43,7 +73,7 @@ selectBtn.addEventListener("click", function () {
     })
     .then(data => {
       // 모달 컨테이너에 jojo.html 콘텐츠를 추가합니다.
-      $("#menu_modalContainer").html(data);
+      $("#modalContainer").html(data);
 
       // detail_menu.css 파일을 로드합니다.
       const linkElement = document.createElement("link");
