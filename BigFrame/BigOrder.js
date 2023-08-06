72ebfda0f674e816d0e1827d89e37766bc5f67ec
jin <jin200126@gmail.com>
2023-08-06 22:52:20 +0900

    20203_8-6

--- BigFrame/BigOrder.js
+++ BigFrame/BigOrder.js
@@ -50,8 +50,8 @@ joImage.addEventListener("click", function () {
       console.error("콘텐츠를 가져오는 중 오류가 발생했습니다:", error);
     });
 });
-// 선택 버튼
 
+// 선택 버튼
 const selectBtn = document.getElementById("selectBtn");
 selectBtn.addEventListener("click", function () {
   // 먼저 모달 컨테이너를 비웁니다.
@@ -63,8 +63,8 @@ selectBtn.addEventListener("click", function () {
     detailMenuLink.remove();
   }
 
-  // jojo.html 콘텐츠를 로드하여 모달 컨테이너에 추가합니다.
-  fetch("../detail_menu/jojo.html")
+  // 외부 detail_menu 폴더에 있는 jojo.html 파일을 로드하여 모달 컨테이너에 추가합니다.
+  fetch("../detail_menu/jojo.html") // 이 부분의 파일 경로를 수정해야합니다.
     .then(response => {
       if (!response.ok) {
         throw new Error("HTTP Error " + response.status);
@@ -75,12 +75,17 @@ selectBtn.addEventListener("click", function () {
       // 모달 컨테이너에 jojo.html 콘텐츠를 추가합니다.
       $("#modalContainer").html(data);
 
-      // detail_menu.css 파일을 로드합니다.
+      // 외부 detail_menu 폴더에 있는 detail_menu.css 파일을 로드합니다.
       const linkElement = document.createElement("link");
       linkElement.rel = "stylesheet";
       linkElement.type = "text/css";
-      linkElement.href = "../detail_menu/detail_menu.css";
+      linkElement.href = "../detail_menu/detail_menu.css"; // 이 부분의 파일 경로를 수정해야합니다.
       document.head.appendChild(linkElement);
+
+      // 외부 detail_menu 폴더에 있는 detail_menu.js 파일을 로드합니다.
+      const scriptElement = document.createElement("script");
+      scriptElement.src = "../detail_menu/detail_menu.js"; // 이 부분의 파일 경로를 수정해야합니다.
+      document.body.appendChild(scriptElement);
 
       const modal = new bootstrap.Modal(document.getElementById("exampleModal"));
       modal.show();
@@ -89,6 +94,8 @@ selectBtn.addEventListener("click", function () {
       console.error("콘텐츠를 가져오는 중 오류가 발생했습니다:", error);
     });
 });
+
+
 
 
 // 검색버튼
--- detail_menu/detail_menu.js
+++ detail_menu/detail_menu.js
@@ -4,20 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
   modal.show();
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var decrementButton = document.getElementById("decrement");
-    var incrementButton = document.getElementById("increment");
-    var quantityInput = document.getElementById("quantity");
-  
-    decrementButton.addEventListener("click", function() {
-      var currentQuantity = parseInt(quantityInput.value);
-      if (currentQuantity > 1) {
-        quantityInput.value = currentQuantity - 1;
-      }
-    });
-  
-    incrementButton.addEventListener("click", function() {
-      var currentQuantity = parseInt(quantityInput.value);
-      quantityInput.value = currentQuantity + 1;
-    });
-  });
+let quantityInput = document.getElementById("input_value");
+
+function increment() {
+  const current = parseInt(quantityInput.value);
+  quantityInput.value = current + 1;
+}
+
+function decrement() {
+  const current = parseInt(quantityInput.value);
+  if (current > 1) {
+    quantityInput.value = current - 1;
+  }
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   var decrementButton = document.getElementById("decrement");
+//   var incrementButton = document.getElementById("increment");
+//   var quantityInput = document.getElementById("quantity");
+
+//   decrementButton.addEventListener("click", function () {
+//     var currentQuantity = parseInt(quantityInput.value);
+//     if (currentQuantity > 1) {
+//       quantityInput.value = currentQuantity - 1;
+//     }
+//   });
+
+//   incrementButton.addEventListener("click", function () {
+//     var currentQuantity = parseInt(quantityInput.value);
+//     quantityInput.value = currentQuantity + 1;
+//   });
+// });
