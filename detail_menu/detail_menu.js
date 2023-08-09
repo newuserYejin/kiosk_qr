c1196c12b18d440f16aa9d51d84b99a2035ae6c8
jin <jin200126@gmail.com>
2023-08-09 18:47:55 +0900

    2023_8_9

--- detail_menu/detail_menu.js
+++ detail_menu/detail_menu.js
@@ -4,34 +4,43 @@ document.addEventListener("DOMContentLoaded", function () {
   modal.show();
 });
 
-let quantityInput = document.getElementById("input_value");
+$(document).ready(function () {
+  $(".input-group").on("click", "#increment", function () {
+    // .input-group 클래스를 가진 요소 내에서 #increment 버튼을 클릭했을 때 실행되는 함수
+    var input = $(this).closest(".input-group").find("input");
+    // 클릭한 버튼이 속한 .input-group 내에서 input 요소를 찾음
+    input.val(parseInt(input.val()) + 1);
+  });
 
-function increment() {
-  const current = parseInt(quantityInput.value);
-  quantityInput.value = current + 1;
-}
+  $(".input-group").on("click", "#decrement", function () {
+    var input = $(this).closest(".input-group").find("input");
+    if (parseInt(input.val()) > 1) {
+      input.val(parseInt(input.val()) - 1);
+    }
+  });
+});
 
-function decrement() {
-  const current = parseInt(quantityInput.value);
-  if (current > 1) {
-    quantityInput.value = current - 1;
-  }
-}
+// (function(){
+//   document.addEventListener("DOMContentLoaded", function () {
+//     var decrementButton = document.getElementById("decrement");
+//     var incrementButton = document.getElementById("increment");
+//     var quantityInput = document.getElementById("input_value");
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   var decrementButton = document.getElementById("decrement");
-//   var incrementButton = document.getElementById("increment");
-//   var quantityInput = document.getElementById("quantity");
+//     decrementButton.addEventListener("click", function () {
+//       var currentQuantity = parseInt(quantityInput.value);
+//       if (currentQuantity > 1) {
+//         quantityInput.value = currentQuantity - 1;
+//         // 변경된 값을 화면에 표시
+//       }
+//       console.log(quantityInput.value);
+//     });
 
-//   decrementButton.addEventListener("click", function () {
-//     var currentQuantity = parseInt(quantityInput.value);
-//     if (currentQuantity > 1) {
-//       quantityInput.value = currentQuantity - 1;
-//     }
+//     incrementButton.addEventListener("click", function () {
+//       var currentQuantity = parseInt(quantityInput.value);
+//       quantityInput.value = currentQuantity + 1;
+//       // 변경된 값을 화면에 표시
+//       quantityInput.dispatchEvent(new Event("input")); // 변경 이벤트 발생
+//     });
 //   });
+// })();
 
-//   incrementButton.addEventListener("click", function () {
-//     var currentQuantity = parseInt(quantityInput.value);
-//     quantityInput.value = currentQuantity + 1;
-//   });
-// });
